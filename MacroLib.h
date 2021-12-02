ec3d94ea991eda813356262fc2d3d0762a45d9a4
Vasco Almeida <l.vasco.m.a21@gmail.com>
2021-12-02 12:23:25 +0000

    Update MacroLib.h
    Added more colors to my library to use with the SetTextColor function

--- MacroLib.h
+++ MacroLib.h
@@ -1,15 +1,27 @@
 #pragma once
 
-//Interface.cpp
+//Colors for use with SetTextColor()
 enum Colors
 {
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Magenta = 5,
+	Yellow = 6,
 	White = 7,
-	Red = 12,
-	Blue = 9,
-	Yellow = 14,
-	Green = 10
+	Gray = 8,
+	Bright_Blue = 9,
+	Bright_Green = 10,
+	Bright_Cyan = 11,
+	Bright_Red = 12,
+	Bright_Magenta = 13,
+	Bright_Yellow = 14,
+	Bright_White = 15
 };
 
+//Interface.cpp
 void Log(const char* message, bool endLine);
 void EndLine();
 int CheckInput(char input[]);
